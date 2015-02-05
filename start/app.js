60745533fc4e6f549ebd8eb18132385636494446
严伟庆 <www.fzw123@163.com>
2015-02-05 15:36:48 +0800

    start to use express with handlebars

--- start/app.js
+++ start/app.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 var debug = require('debug')('start');
 
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,14 +8,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var exphbs = require('express-handlebars');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
