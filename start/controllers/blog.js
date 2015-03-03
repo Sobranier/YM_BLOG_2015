0f21b0adcee89011434d2b26dfd54f8a5fe9a257
严伟庆 <www.fzw123@163.com>
2015-03-03 16:49:20 +0800

    save for topic & tag router completed

--- start/controllers/blog.js
+++ start/controllers/blog.js
@@ -9,39 +9,8 @@ Topic = mongoose.model('Topic');
 
 module.exports = function (app) {
     app.get('/', function(req, res) {
-        /*
-        var newBlog = new Blog({
-            title: '测试0018',
-            date: new Date(),
-            content: '本文记录了美团在推广webp的一方面实践，虽然本人负责主要的实施，但领导及同事们提供了很大的帮助，包括方案的讨论及选定和后期文章的审阅等等。本文除了大记录了如何实施，还分享了整个效果评估的方案，总的来说是对新技术的一些尝试。更多内容可以去http://fe.meituan.com观看。',
-            summary: '这是测试的贱贱贱加加加加123',
-            alias: 'test0018',
-            topics: ['JavaScript', 'life0003'],
-            tags: ['Html', 'JavaScript', 'Node', '前端', 'life0002'],
-            ifpublic: true,
-            ifsafe: true
-        });
-        newBlog.save();
-        */
-
-        var page = req.query.p ? parseInt(req.query.p) : 1;
-
         Blog.count({}, function (err, total) {
-            Blog.find({}).sort({'date':-1}).skip(10*(page-1)).limit(10).exec(function (err, blogs) {
-                var notFirstPage = ((page - 1) != 0),
-                    notLastPage = (((page - 1) * 10 + blogs.length) != total),
-                    relatePage = {};
-                if (notFirstPage) {
-                    relatePage.notFirstPage = true;
-                }
-                if (notLastPage) {
-                    relatePage.notLastPage = true;
-                }
-                for (var index in blogs) {
-                    var date = blogs[index].date;
-                    blogs[index].day = (date.getMonth() + 1) + "-" + date.getDate();
-                    blogs[index].year = date.getFullYear();
-                }
+            Blog.find({}).sort({'date':-1}).limit(3).exec(function (err, blogs) {
                 res.render('home', {
                     title: 'YANWEIQING',
                     file: {
@@ -53,18 +22,84 @@ module.exports = function (app) {
                             '测试测试测试3',
                         ]
                     },
-                    posts: blogs,
-                    relatePage: relatePage
+                    posts: blogs
                 });
             });
         });
 
     });
 
+    // 搜索
     app.post('/posts/search', function (req, res) {
-        getPaperList(res, req.body);
+        getPaperList(res, req.body, true);
     });
 
+    // blog列表界面
+    app.get('/blog', function (req, res) {
+        var params = {
+            page: req.query.page ? parseInt(req.query.page) : 1,
+            ifpublic: true,
+            data: {
+                title: '日志 - 寿百年',
+                file: {
+                    name: '所有日志',
+                    title: '所有日志',
+                    content: [
+                        '测试测试测试1',
+                        '测试测试测试2',
+                        '测试测试测试3',
+                    ]
+                }
+            }
+        }
+        getPaperList(res, params, false);
+    });
+
+    // tag列表界面
+    app.get('/tags/:tag', function (req, res) {
+        var params = {
+            page: req.query.page ? parseInt(req.query.page) : 1,
+            tag: req.params.tag,
+            ifpublic: true,
+            data: {
+                title: req.params.tag + ' - 寿百年',
+                file: {
+                    title: '随笔测试',
+                    content: [
+                        '你好吗',
+                        '你妹',
+                        '你'
+                    ]
+                },
+                kind: '文章列表'
+            }
+        }
+        getPaperList(res, params, false);
+    });
+
+    // topic列表界面
+    app.get('/topics/:topic', function (req, res) {
+        Topic.findOne({name: req.params.topic}).exec(function (err, topic) {
+            if (topic) {
+                var params = {
+                    page: req.query.page ? parseInt(req.query.page) : 1,
+                    topic: req.params.topic,
+                    ifpublic: true,
+                    data: {
+                        title: topic.name + ' - 寿百年',
+                        file: topic,
+                        kind: '文章列表'
+                    }
+                }
+                getPaperList(res, params, false);
+            } else {
+                // 此类重定向未来需要重新考虑
+                res.redirect('/topics'); 
+            }
+        });
+    })
+
+    // 单个文章界面（可以和后台preview界面做一些联系）
     app.get('/posts/:alias', function (req, res) {
         Blog.find({alias: req.params.alias}).exec(function (err, blogs) {
             var date = blogs[0].date;
@@ -77,7 +112,6 @@ module.exports = function (app) {
         });
     });
 
-
     // 前台标签页面
     app.get('/tags', function (req, res) {
         Tag.find({}).exec(function (err, tags) {
@@ -86,21 +120,6 @@ module.exports = function (app) {
                 tags: tags
             });
         });
-    });
-    app.get('/tags/:tag', function (req, res) {
-        console.log(req.params.tag);
-        res.render('front/paperlist', {
-            title: req.params.tag + ' - 寿百年',
-            file: {
-                title: '随笔测试',
-                content: [
-                    '你好吗',
-                    '你妹',
-                    '你'
-                ]
-            },
-            kind: '文章列表'
-        }); 
     });
 
     // 前台分类页面
@@ -112,20 +131,6 @@ module.exports = function (app) {
             });
         });
     });
-    app.get('/topics/:topic', function (req, res) {
-        console.log(req.params.topic);
-        Topic.find({name: req.params.topic}).exec(function (err, topic) {
-            if (topic.length > 0) {
-                res.render('front/paperlist', {
-                    title: topic[0].name + ' - 寿百年',
-                    file: topic[0],
-                    kind: '分类列表'
-                });
-            } else {
-                res.redirect('/'); 
-            }
-        });
-    })
 
     // 前台历史
     app.get('/archives', function (req, res) {
@@ -135,34 +140,62 @@ module.exports = function (app) {
     });
 
     // 获取文章列表的函数
-    function getPaperList (res, params) {
-        var pageNum = params.page ? params.page : 1,
-            re = new RegExp(params.title, 'i');
+    function getPaperList (res, params, isAjax) {
+        var pageNum = params.page ? parseInt(params.page) : 1,
+            re = new RegExp(params.title, 'i'),
+            pageSize = 10;
+        
+        // 过滤条件
+        var config = {};
+        config.ifpublic = params.ifpublic;
+        config.title = {$regex: re};
+        if (params.tag) {
+            config.tags = params.tag;
+        }
+        if (params.topic) {
+            config.topics = params.topic;
+        }
+        console.log(config);
 
-        Blog.count({'ifpublic': params.ifpublic, 'title': {$regex: re}}, function (err, total) {
-            var number = Math.ceil(total/10);
+        Blog.count(config, function (err, total) {
+            // 保障页码合法性
+            var number = Math.ceil(total/pageSize); //总页码数
             pageNum = (pageNum < 1) ? 1 : pageNum;
-            pageNum = (pageNum > number) ? number : pageNum;
+            pageNum = (pageNum > number) ? number : pageNum;    //当前页码
 
-            Blog.find({'ifpublic': params.ifpublic, 'title': {$regex: re}}).sort({'date':-1}).skip(10*(pageNum-1)).limit(10).exec(function (err, blogs) {
+            Blog.find(config).sort({'date':-1}).skip(pageSize*(pageNum-1)).limit(pageSize).exec(function (err, blogs) {
+
                 for (var index in blogs) {
                     var date = blogs[index].date;
                     blogs[index].day = (date.getMonth() + 1) + "-" + date.getDate();
                     blogs[index].year = date.getFullYear();
                 }
-                res.json({
-                    posts: blogs,
-                    page: {
-                        total: total,
-                        pageNum: pageNum,
-                        number: Math.ceil(total/10)
-                    }
-                });
 
+                if (isAjax) {
+                    res.json({
+                        posts: blogs,
+                        page: {
+                            total: total,
+                            pageNum: pageNum,
+                            number: Math.ceil(total/pageSize)
+                        }
+                    });
+                } else {
+                    var notFirstPage = ((pageNum - 1) != 0),
+                        notLastPage = (pageNum != number),
+                        relatePage = {};
+                    if (notFirstPage) {
+                        relatePage.notFirstPage = true;
+                    }
+                    if (notLastPage) {
+                        relatePage.notLastPage = true;
+                    }
+                
+                    params.data.posts = blogs;
+                    params.data.relatePage = relatePage;
+                    res.render('front/paperlist', params.data);
+                }
             });
         });
-
-
-
     }
 }
