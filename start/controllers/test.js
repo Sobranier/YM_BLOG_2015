e6e295135e198eaa8b433e8c92ac96eca8fde12c
严伟庆 <www.fzw123@163.com>
2015-03-31 10:45:50 +0800

    save for music page first start

---
+++ start/controllers/test.js
@@ -0,0 +1,26 @@
+var cheerio = require('cheerio'),
+    superagent = require('superagent');
+
+
+module.exports = function (app) {
+    app.get('/tt', function(req, res, next) {
+        superagent.get('https://cnodejs.org/')
+            .end(function(err, sres) {
+                if (err) {
+                    return next(err);
+                }
+                var $ = cheerio.load(sres.text);
+                var items = [];
+                $('#topic_list .topic_title').each(function (idx, element) {
+                    var $element = $(element);
+                    items.push({
+                        title: $element.attr('title'),
+                        href: $element.attr('href')
+                    });
+                });
+
+                res.send(items);
+            });
+    });
+
+};
