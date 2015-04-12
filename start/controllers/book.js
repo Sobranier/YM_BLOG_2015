289f5983fc2121339e5a2b0b33af89e6909be8cb
严伟庆 <www.fzw123@163.com>
2015-04-12 16:06:00 +0800

    save for come advance on first page

--- start/controllers/book.js
+++ start/controllers/book.js
@@ -4,12 +4,21 @@ var Test = require('../models/test'),
 module.exports = function (app) {
     
     app.get('/booklist', function (req, res) {
+        var bookStatus = {
+            'wish': '想读',
+            'reading': '在读',
+            'read': '读过'
+        };
         superagent.get('https://api.douban.com/v2/book/user/sobranier/collections')
         .end(function(err, sres) {
             if (err) {
                 return next(err);
             }
             var items = JSON.parse(sres.text).collections;
+            for (var i = 0, length = items.length; i < length; i ++) {
+                items[i].status = bookStatus[items[i].status];
+            }
+            
             res.render('pages/booklist', {
                 info: items,
                 title: '伟庆书单'
